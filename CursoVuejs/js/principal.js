e433081902bebd5836d38fbddcd862b1c03afd8d
garduino <garduino@gmail.com>
2018-07-22 20:13:07 -0300

    First Commit.

---
+++ CursoVuejs/js/principal.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+
+el : '#app',
+data : {
+
+    lista : [
+
+        {nombre : 'Juan Carlos', promedio : '10', estado : false},
+        {nombre : 'Liliana Nataly', promedio : '17', estado : true},
+        {nombre : 'Luis Alberto', promedio : '18', estado : true}
+    ],
+
+}
+
+
+})
