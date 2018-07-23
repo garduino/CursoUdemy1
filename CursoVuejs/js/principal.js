be01479ae2263b4e646e9d3118637991898ae431
garduino <garduino@gmail.com>
2018-07-22 21:41:40 -0300

    Lista dinámica.

--- CursoVuejs/js/principal.js
+++ CursoVuejs/js/principal.js
@@ -5,10 +5,30 @@ data : {
 
     lista : [
 
-        {nombre : 'Juan Carlos', promedio : '10', estado : false},
-        {nombre : 'Liliana Nataly', promedio : '17', estado : true},
-        {nombre : 'Luis Alberto', promedio : '18', estado : true}
     ],
+        nombre : '',
+        promedio : ''
+},
+methods : {
+
+    agregarNota: function() {
+
+        estadoA = false;
+        if (this.promedio > 12.5){
+            estadoA = true;
+        }
+        if (this.nombre!='' && this.promedio!=''){
+
+this.lista.push({nombre:this.nombre, promedio:this.promedio, estado:estadoA});
+this.nombre="";
+this.promedio="";
+        }
+        else {
+            alert("Ingrese el nombre y promedio del estudiante");
+        }
+ 
+ 
+    }
 
 }
 
